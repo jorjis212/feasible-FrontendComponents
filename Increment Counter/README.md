9cba204c7450baa1f48409028e49ef00d3c0ce0f
jorjis <devjorjis212@gmail.com>
2022-03-22 00:11:55 +0600

    add Increment Counter

---
+++ Increment Counter/README.md
@@ -0,0 +1,9 @@
+![project-overview](./dist/project-snap.png)
+
+# This project is Live [here]()
+
+### Script Overview :
+
+```javascript
+recursive function, data-attributes, basic DOM manipulation
+```
---
+++ Increment Counter/dist/script/app.js
@@ -0,0 +1,24 @@
+
+'use strict';
+
+const counters =
+    document.querySelectorAll('.container__counter__count');
+
+counters.forEach(item => {
+    item.innerHTML = '00';
+
+    function updateCounter() {
+        const max = +item.getAttribute('data-target');
+        const temp = +item.innerHTML;
+
+        const increment = max / 100;
+
+        if (temp < max) {
+            item.innerHTML = `${Math.ceil(temp + increment)}`;
+            setTimeout(updateCounter, 1);
+        } else {
+            item.innerHTML = max;
+        }
+    }
+    updateCounter();
+});
